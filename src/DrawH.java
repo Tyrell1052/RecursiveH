127c1f75857e11b721368da2ae1ce1365d097b7e
Tyrell <Tyrell102@gmail.com>
2019-02-04 16:01:23 -0500

    saving

--- src/DrawH.java
+++ src/DrawH.java
@@ -1,32 +1,16 @@
-/* DrawHdemo.Java
- * Computer Science 112, Spring 2014
- * Last edited Jan. 20, 2014 by C. Herbert
- * 
- * This code demonstrates how to draw an H-shaped fractal antenna 
- * using Java Graphics class objects and recursion
- * 
- * It has two mutually recursive methods to draw the horizontal lines 
- * and vertical line of an H to form an H-shaped antenna.
- * 
- * The end points of a line in one direction become the midpoints for two 
- * lines in the other diirection.
- * 
- * The recursion starts wiht a horizontal line 200 units long and stops
- * when the line length would be less than 10
- * 
- * It has five levels of recursion, with lines of length 
- * 
- * 200 - 100 - 50 - 25 - 12
- * 
+/* DrawH.Java
+ * Author: Tyrell Robbins
+ *
+ * This program will demonstrates how to take a program from using mutual recursion
+ * to having the recursive call within one methods instead of two that will perform the same ending result
+ *
  */
-package drawhdemo;
 
 import java.awt.*;
 import javax.swing.*;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
-public class DrawHdemo {
+public class DrawH{
 
     public static void main(String[] args) {
 
@@ -50,24 +34,23 @@ public class DrawHdemo {
 
 class HCanvas extends Canvas {
 
-
     // HCanvas() constructor
     public HCanvas() {
     }
 
     public void paint(Graphics graphics) {
-    double x1;      // x coordinate for point 1
-    double y1;      // y coordinate for point 1
-    double x2;      // x coordinate for point 2
-    double y2;      // y coordinate for point 2
-    
-    //set the midpoint for the first line in the center of the canvas
-    double xMid = 250;
-    double yMid = 250;
-    double length = 200;      // the length of the first line to be drawn
+        double x1;      // x coordinate for point 1
+        double y1;      // y coordinate for point 1
+        double x2;      // x coordinate for point 2
+        double y2;      // y coordinate for point 2
 
-        String title;       // a ttitle for the image on the canvas
-        
+        //set the midpoint for the first line in the center of the canvas
+        double xMid = 250;
+        double yMid = 250;
+        double length = 200;      // the length of the first line to be drawn
+
+        String title;       // a title for the image on the canvas
+
         // Put a title on the Canvas
         graphics.setColor(Color.BLUE);
         graphics.setFont(new Font("Arial", Font.PLAIN, 18));
@@ -80,13 +63,12 @@ class HCanvas extends Canvas {
         //draw the first line
         drawH(graphics, xMid, yMid, length );
 
-        
     }  // end paint()
 
 
     //***********************************************************************************************
 
-    public void drawH(Graphics graphics, double xMid, double yMid, double length)
+    private void drawH(Graphics graphics, double xMid, double yMid, double length)
     {
         // find horizontal left endpoint
         double x1 = xMid - (length / 2);
@@ -95,7 +77,6 @@ class HCanvas extends Canvas {
         // find horizontal right endpoint
         double x2 = xMid + (length / 2);
         double y2 = yMid;
-
 
         // find lower left vertical endpoint
         double x3 = x1;
@@ -113,38 +94,31 @@ class HCanvas extends Canvas {
         double x6 = x2;
         double y6 = yMid + (length / 2);
 
-//        graphics.drawLine((int) x1, (int) y1, (int) x2, (int) y2);
-//        graphics.drawLine((int) x3, (int) y3, (int) x4, (int) y4);
 
-        if (length > 5)
+        if (length > 5) // this will allow the recursive method to run until there is only 5 pixels between the H
         {
             graphics.drawLine((int) x1, (int) y1, (int) x2, (int) y2);
-            try {
-                Thread.sleep(1000);
+            try {// this try an catch statement will allow me to add a time delay
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             graphics.drawLine((int) x3, (int) y3, (int) x4, (int) y4);
-            try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {// this try an catch statement will allow me to add a time delay
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             graphics.drawLine((int) x5, (int) y5, (int) x6, (int) y6);
 
+            // calling these drawH methods will start the recursion
             drawH(graphics,(int) x3, (int) y3,(length/2));
             drawH(graphics,(int) x4, (int) y4,(length/2));
             drawH(graphics,(int) x5, (int) y5,(length/2));
             drawH(graphics,(int) x6, (int) y6,(length/2));
 
-
-
         }//end if
 
     } // end drawH
 
-
 } // end class MyCanvas
-
-
-
